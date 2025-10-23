109d07579d2ff88d38dba519780a54fbcf7be2d5
Sriram Katta <sriram.katta.939@gmail.com>
2025-10-23 20:59:54 +0200

    Wrap MPI_Comm_size and MPI_Comm_rank calls with MPI_CALL for error handling

--- A02/c-skeleton/src/main.c
+++ A02/c-skeleton/src/main.c
@@ -43,8 +43,8 @@ int main(int argc, char** argv) {
     int rank = 0;
     int size = 1;
     MPI_CALL(MPI_Init(&argc, &argv));
-    MPI_Comm_size(MPI_COMM_WORLD, &size); 
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_CALL(MPI_Comm_size(MPI_COMM_WORLD, &size)); 
+    MPI_CALL(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
     printf("Hello World! I am %d of %d\n", rank, size);
     MPI_CALL(MPI_Finalize());
     return EXIT_SUCCESS; }
