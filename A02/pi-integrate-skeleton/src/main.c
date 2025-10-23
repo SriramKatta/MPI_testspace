7eba76d40800e2a618cd5b1f9e03ae07add2ffc7
Sriram Katta <sriram.katta.939@gmail.com>
2025-10-23 21:11:45 +0200

    chkpoint

--- A02/pi-integrate-skeleton/src/main.c
+++ A02/pi-integrate-skeleton/src/main.c
@@ -12,14 +12,53 @@
 
 #include "timing.h"
 
+
+#define DX 1e-3;
+
+#define MPI_CALL(call)                                                                \
+    {                                                                                 \
+        int mpi_status = call;                                                        \
+        if (MPI_SUCCESS != mpi_status) {                                              \
+            char mpi_error_string[MPI_MAX_ERROR_STRING];                              \
+            int mpi_error_string_length = 0;                                          \
+            MPI_Error_string(mpi_status, mpi_error_string, &mpi_error_string_length); \
+            if (NULL != mpi_error_string)                                             \
+                fprintf(stderr,                                                       \
+                        "ERROR: MPI call \"%s\" in line %d of file %s failed "        \
+                        "with %s "                                                    \
+                        "(%d).\n",                                                    \
+                        #call, __LINE__, __FILE__, mpi_error_string, mpi_status);     \
+            else                                                                      \
+                fprintf(stderr,                                                       \
+                        "ERROR: MPI call \"%s\" in line %d of file %s failed "        \
+                        "with %d.\n",                                                 \
+                        #call, __LINE__, __FILE__, mpi_status);                       \
+            exit( mpi_status );                                                       \
+        }                                                                             \
+    }
+
+
 double integrate(double, double);
 
 int main (int argc, char** argv) {
     double wcs, wce;
     double  Pi;
 
+    int rank = 0;
+    int size = 1;
+    MPI_CALL(MPI_Init(&argc, &argv));
+    MPI_CALL(MPI_Comm_size(MPI_COMM_WORLD, &size)); 
+    MPI_CALL(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+
+    double chunk = 1.0 / size;
+    double start = rank * chunk;
+    double end = start + chunk;
+
     wcs = getTimeStamp();
-    Pi = integrate(0.0, 1.0);
+    double Pi_local = integrate(start, chunk);
+
+    MPI_CALL(MPI_Barrier(MPI_COMM_WORLD));
+
     wce = getTimeStamp();
 
     printf("Pi=%.15lf in %.3lf s \n", Pi,wce-wcs);
@@ -36,5 +75,11 @@ double integrate(double a, double b) {
 		Return sum * delta x to get the value of PI.
 		
 	*/
-	return 0.0;
+    double res = 0.0;
+    while(a < b){
+        res += sqrt(1-a*a);
+        a += DX;
+    }
+
+	return res * DX;
 }
