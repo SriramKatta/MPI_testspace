7e821c3cdf0f0001e8aaa08b7486b690eb5329fb
Sriram Katta <sriram.katta.939@gmail.com>
2025-11-06 19:41:36 +0100

    Refactor MPI_CALL macro for improved error handling and consistency

--- A03/src/util.h
+++ A03/src/util.h
@@ -13,27 +13,28 @@
   "--------------------------------------------------------------------------" \
   "--\n"
 
-#define MPI_CALL(call)                                                         \
-  {                                                                            \
-    int mpi_status = call;                                                     \
-    if (MPI_SUCCESS != mpi_status) {                                           \
-      char mpi_error_string[MPI_MAX_ERROR_STRING];                             \
-      int mpi_error_string_length = 0;                                         \
-      MPI_Error_string(mpi_status, mpi_error_string,                           \
-                       &mpi_error_string_length);                              \
-      if (NULL != mpi_error_string)                                            \
-        fprintf(stderr,                                                        \
-                "ERROR: MPI call \"%s\" in line %d of file %s failed "         \
-                "with %s "                                                     \
-                "(%d).\n",                                                     \
-                #call, __LINE__, __FILE__, mpi_error_string, mpi_status);      \
-      else                                                                     \
-        fprintf(stderr,                                                        \
-                "ERROR: MPI call \"%s\" in line %d of file %s failed "         \
-                "with %d.\n",                                                  \
-                #call, __LINE__, __FILE__, mpi_status);                        \
-      exit(mpi_status);                                                        \
-    }                                                                          \
+#define MPI_CALL(MPIFUNCCALL)                                             \
+  {                                                                       \
+    int mpi_status = MPIFUNCCALL;                                         \
+    if (MPI_SUCCESS != mpi_status)                                        \
+    {                                                                     \
+      char mpi_error_string[MPI_MAX_ERROR_STRING];                        \
+      int mpi_error_string_length = 0;                                    \
+      MPI_Error_string(mpi_status, mpi_error_string,                      \
+                       &mpi_error_string_length);                         \
+      if (NULL != mpi_error_string)                                       \
+        fprintf(stderr,                                                   \
+                "ERROR: MPI call \"%s\" in line %d of file %s failed "    \
+                "with %s "                                                \
+                "(%d).\n",                                                \
+                #MPIFUNCCALL, __LINE__, __FILE__, mpi_error_string, mpi_status); \
+      else                                                                \
+        fprintf(stderr,                                                   \
+                "ERROR: MPI call \"%s\" in line %d of file %s failed "    \
+                "with %d.\n",                                             \
+                #MPIFUNCCALL, __LINE__, __FILE__, mpi_status);                   \
+      exit(mpi_status);                                                   \
+    }                                                                     \
   }
 
 #ifndef MIN
@@ -46,13 +47,15 @@
 #define ABS(a) ((a) >= 0 ? (a) : -(a))
 #endif
 
-inline int rows_in_rank(int rank, int size, int N) {
+inline int rows_in_rank(int rank, int size, int N)
+{
   int basecount = N / size;
   int remainder = N % size;
   return basecount + ((remainder > rank) ? 1 : 0);
 }
 
-inline int rows_start_of_rank(int rank, int size, int N) {
+inline int rows_start_of_rank(int rank, int size, int N)
+{
   int basecount = N / size;
   int remainder = N % size;
   return basecount * rank + MIN(rank, remainder);
