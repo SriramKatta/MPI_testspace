8ba27436238a43f00efea61d3ca18e8eef9c4d58
Sriram Katta <sriram.katta.939@gmail.com>
2025-11-10 17:07:26 +0100

    update

--- A03/src/dmvm.c
+++ A03/src/dmvm.c
@@ -10,6 +10,19 @@
 
 #include "util.h"
 
+inline void dmvm_core(int Nlocal, int cs, int currentN,
+                      double *restrict y, const double *restrict a, int N,
+                      double *restrict x)
+{
+  for (int r = 0; r < Nlocal; r++)
+  {
+    for (int c = cs; c < cs + currentN; c++)
+    {
+      y[r] = y[r] + a[r * N + c] * x[c - cs];
+    }
+  }
+}
+
 double dmvm(double *restrict y,
             const double *restrict a,
             double *restrict x,
@@ -18,71 +31,64 @@ double dmvm(double *restrict y,
             int iter)
 {
 
-    int rank, size;
+  int rank, size;
 
-    MPI_CALL(MPI_Comm_size(MPI_COMM_WORLD, &size));
-    MPI_CALL(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+  MPI_CALL(MPI_Comm_size(MPI_COMM_WORLD, &size));
+  MPI_CALL(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
 
-    int num = N / size;
-    int rest = N % size;
+  int num = N / size;
+  int rest = N % size;
 
-    int upperNeighbor = upper_nbr(rank, size);
-    int lowerNeighbor = lower_nbr(rank, size);
+  int upperNeighbor = upper_nbr(rank, size);
+  int lowerNeighbor = lower_nbr(rank, size);
 
-    int cs = rows_start_of_rank(rank, size, N);
+  int cs = rows_start_of_rank(rank, size, N);
 
-    double ts = MPI_Wtime();
-    for (int j = 0; j < iter; j++)
+  double ts = MPI_Wtime();
+  for (int j = 0; j < iter; j++)
+  {
+    int currentN = Nlocal;
+    int rankCurrent = rank;
+
+    // loop over RHS ring shifts
+    for (int rot = 0; rot < size; rot++)
     {
-        int currentN = Nlocal;
-        int rankCurrent = rank;
 
-        // loop over RHS ring shifts
-        for (int rot = 0; rot < size; rot++)
+      // local DMVM
+      dmvm_core(Nlocal, cs, currentN, y, a, N, x);
+
+      cs += currentN;
+      if (cs >= N)
+      {
+        cs = 0;
+      }
+
+      rankCurrent = lower_nbr(rankCurrent, size);
+
+      currentN = rows_in_rank(rankCurrent, size, N);
+
+      if (rot != (size - 1))
+      {
+        if (rank % 2) // to prevent dead locking
         {
+          MPI_CALL(MPI_Send(x, num + (rest ? 1 : 0), MPI_DOUBLE,
+                            upperNeighbor, 0, MPI_COMM_WORLD));
 
-            // local DMVM
-            for (int r = 0; r < Nlocal; r++)
-            {
-                for (int c = cs; c < cs + currentN; c++)
-                {
-                    y[r] = y[r] + a[r * N + c] * x[c - cs];
-                }
-            }
-
-            // ringshift communication
-            cs += currentN;
-            if (cs >= N)
-            {
-                cs = 0; // wrap around
-            }
-
-            rankCurrent = lower_nbr(rankCurrent, size);
-
-            currentN = rows_in_rank(rankCurrent, size, N);
-
-            if (rot != (size - 1))
-            {
-                if (rank % 2) // to prevent dead locking
-                {
-                    MPI_CALL(MPI_Send(x, num + (rest ? 1 : 0), MPI_DOUBLE,
-                                      upperNeighbor, 0, MPI_COMM_WORLD));
-
-                    MPI_CALL(MPI_Recv(x, num + (rest ? 1 : 0), MPI_DOUBLE,
-                                      lowerNeighbor, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE));
-                }
-                else
-                {
-                    MPI_CALL(MPI_Recv(x, num + (rest ? 1 : 0), MPI_DOUBLE,
-                                      lowerNeighbor, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE));
-
-                    MPI_CALL(MPI_Send(x, num + (rest ? 1 : 0), MPI_DOUBLE,
-                                      upperNeighbor, 0, MPI_COMM_WORLD));
-                }
-            }
+          MPI_CALL(MPI_Recv(x, num + (rest ? 1 : 0), MPI_DOUBLE,
+                            lowerNeighbor, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE));
         }
-    }
-    double te = MPI_Wtime();
+        else
+        {
+          MPI_CALL(MPI_Recv(x, num + (rest ? 1 : 0), MPI_DOUBLE,
+                            lowerNeighbor, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE));
 
-    return te - ts;
+          MPI_CALL(MPI_Send(x, num + (rest ? 1 : 0), MPI_DOUBLE,
+                            upperNeighbor, 0, MPI_COMM_WORLD));
+        }
+      }
+    }
+  }
+  double te = MPI_Wtime();
+
+  return te - ts;
 }
--- A03/src/main.c
+++ A03/src/main.c
@@ -22,60 +22,59 @@ extern double dmvm(
     int Nlocal,
     int iter);
 
-void dmvm_core(int Nlocal, int cs, int currentN, double *__restrict__ y, const double *__restrict__ a, int N, double *__restrict__ x);
-
 int main(int argc, char **argv)
 {
-    size_t bytesPerWord = sizeof(double);
-    size_t N = 0;
-    size_t Nlocal = 0;
-    size_t iter = 1;
-    int rank, size;
+  size_t bytesPerWord = sizeof(double);
+  size_t N = 0;
+  size_t Nlocal = 0;
+  size_t iter = 1;
+  int rank, size;
 
-    MPI_CALL(MPI_Init(&argc, &argv));
-    MPI_CALL(MPI_Comm_size(MPI_COMM_WORLD, &size));
-    MPI_CALL(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+  MPI_CALL(MPI_Init(&argc, &argv));
+  MPI_CALL(MPI_Comm_size(MPI_COMM_WORLD, &size));
+  MPI_CALL(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
 
+  if (argc != 3)
+  {
     if (rank == 0)
     {
-        if (argc != 3)
-        {
-            printf("Usage: %s <N> <iter>\n", argv[0]);
-            exit(EXIT_SUCCESS);
-        }
+      printf("Usage: %s <N> <iter>\n", argv[0]);
     }
-
-    N = atoi(argv[1]);
-    iter = atoi(argv[2]);
-    Nlocal = rows_in_rank(rank, size, N);
-
-    double *a = (double *)allocate(ARRAY_ALIGNMENT, N * Nlocal * bytesPerWord);
-    double *x = (double *)allocate(ARRAY_ALIGNMENT, (Nlocal + 1) * bytesPerWord);
-    double *y = (double *)allocate(ARRAY_ALIGNMENT, (Nlocal + 1) * bytesPerWord);
-
-    // initialize arrays
-    int cs = rows_start_of_rank(rank, size, N);
-
-    for (int i = 0; i < Nlocal; i++)
-    {
-        x[i] = (double)(cs + i);
-        y[i] = 0.0;
-
-        for (int j = 0; j < N; j++)
-        {
-            a[i * N + j] = (double)(j + cs + i);
-        }
-    }
-
-    double walltime = dmvm(y, a, x, N, Nlocal, iter);
-
-    double flops = (double)2 * N * N * iter;
-    if (rank == 0)
-    {
-        printf("RES | %zu %d %zu %.2f %.2f\n", iter, size, N, 1.0E-06 * flops / walltime,
-               walltime);
-    }
-
     MPI_CALL(MPI_Finalize());
-    return EXIT_SUCCESS;
+    exit(EXIT_SUCCESS);
+  }
+
+  N = atoi(argv[1]);
+  iter = atoi(argv[2]);
+  Nlocal = rows_in_rank(rank, size, N);
+
+  double *a = (double *)allocate(ARRAY_ALIGNMENT, N * Nlocal * bytesPerWord);
+  double *x = (double *)allocate(ARRAY_ALIGNMENT, (Nlocal + 1) * bytesPerWord);
+  double *y = (double *)allocate(ARRAY_ALIGNMENT, (Nlocal + 1) * bytesPerWord);
+
+  // initialize arrays
+  int cs = rows_start_of_rank(rank, size, N);
+
+  for (int i = 0; i < Nlocal; i++)
+  {
+    x[i] = (double)(cs + i);
+    y[i] = 0.0;
+
+    for (int j = 0; j < N; j++)
+    {
+      a[i * N + j] = (double)(j + cs + i);
+    }
+  }
+
+  double walltime = dmvm(y, a, x, N, Nlocal, iter);
+
+  double flops = (double)2 * N * N * iter;
+  if (rank == 0)
+  {
+    printf("RES | %zu %d %zu %.2f %.2f\n", iter, size, N, 1.0E-06 * flops / walltime,
+           walltime);
+  }
+
+  MPI_CALL(MPI_Finalize());
+  return EXIT_SUCCESS;
 }
