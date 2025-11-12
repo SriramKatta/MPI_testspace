b7335f3121d8f5b2e0212576106abf0220aec7e4
Sriram Katta <sriram.katta.939@gmail.com>
2025-11-12 13:35:47 +0100

    Refactor main function for MPI impl

--- A04/src/main.c
+++ A04/src/main.c
@@ -9,33 +9,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <mpi.h>
 
 #include "likwid-marker.h"
 #include "parameter.h"
 #include "solver.h"
 #include "timing.h"
+#include "util.h"
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    double startTime, endTime;
+
+    MPI_CALL(MPI_Init(&argc, &argv));
+    int rank, size;
+    MPI_CALL(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+    MPI_CALL(MPI_Comm_size(MPI_COMM_WORLD, &size));
+
     Parameter params;
     Solver solver;
     initParameter(&params);
 
-    if (argc < 2) {
-        printf("Usage: %s <configFile>\n", argv[0]);
+    if (argc < 2)
+    {
+        if (rank == 0)
+        {
+            printf("Usage: %s <configFile>\n", argv[0]);
+        }
+        MPI_CALL(MPI_Finalize());
         exit(EXIT_SUCCESS);
     }
+
     readParameter(&params, argv[1]);
-    printParameter(&params);
+
+    if (rank == 0)
+    {
+        printParameter(&params);
+    }
 
     initSolver(&solver, &params, 2);
-    startTime = getTimeStamp();
+    double startTime = getTimeStamp();
     solve(&solver);
-    endTime = getTimeStamp();
+    double endTime = getTimeStamp();
     writeResult(&solver, "p.dat");
-
-    printf("Walltime %.2fs\n", endTime - startTime);
-
+    if (rank == 0)
+    {
+        printf("Walltime %.2fs\n", endTime - startTime);
+    }
+    
+    MPI_CALL(MPI_Finalize());
     return EXIT_SUCCESS;
 }
