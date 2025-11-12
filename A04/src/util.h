4e0c82933c5aa1d607b0ed7d139ca53b86029f6c
Sriram Katta <sriram.katta.939@gamil.com>
2025-11-12 08:09:36 +0100

    Update MPI compiler settings and enhance error handling in utility functions

--- A04/src/util.h
+++ A04/src/util.h
@@ -6,6 +6,9 @@
  */
 #ifndef __UTIL_H_
 #define __UTIL_H_
+
+#include <mpi.h>
+
 #define HLINE "----------------------------------------------------------------------------\n"
 
 #ifndef MIN
@@ -16,6 +19,41 @@
 #endif
 #ifndef ABS
 #define ABS(a) ((a) >= 0 ? (a) : -(a))
+#endif
+
+
+#ifdef NDEBUG
+
+#define MPI_CALL(MPIFUNCCALL) \
+  {                           \
+    MPIFUNCCALL;              \
+  }
+
+#else
+
+#define MPI_CALL(MPIFUNCCALL)                                                    \
+  {                                                                              \
+    int mpi_status = MPIFUNCCALL;                                                \
+    if (MPI_SUCCESS != mpi_status)                                               \
+    {                                                                            \
+      char mpi_error_string[MPI_MAX_ERROR_STRING];                               \
+      int mpi_error_string_length = 0;                                           \
+      MPI_Error_string(mpi_status, mpi_error_string,                             \
+                       &mpi_error_string_length);                                \
+      if (NULL != mpi_error_string)                                              \
+        fprintf(stderr,                                                          \
+                "ERROR: MPI call \"%s\" in line %d of file %s failed "           \
+                "with %s "                                                       \
+                "(%d).\n",                                                       \
+                #MPIFUNCCALL, __LINE__, __FILE__, mpi_error_string, mpi_status); \
+      else                                                                       \
+        fprintf(stderr,                                                          \
+                "ERROR: MPI call \"%s\" in line %d of file %s failed "           \
+                "with %d.\n",                                                    \
+                #MPIFUNCCALL, __LINE__, __FILE__, mpi_status);                   \
+      exit(mpi_status);                                                          \
+    }                                                                            \
+  }
 #endif
 
 #endif // __UTIL_H_
