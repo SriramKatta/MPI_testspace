bf36edb78d68cbd6be9f35ccc5a3c7bf07a3b7e5
Sriram Katta <sriram.katta.939@gmail.com>
2025-12-03 21:58:35 +0100

    A06 init

---
+++ A06/sequential/include_GCC.mk
@@ -0,0 +1,14 @@
+CC   = mpicc
+GCC  = mpicc
+LINKER = $(CC)
+
+ifeq ($(ENABLE_OPENMP),true)
+OPENMP   = -fopenmp
+endif
+
+VERSION  = --version
+CFLAGS   = -Ofast -ffreestanding -std=c99 $(OPENMP)
+LFLAGS   = $(OPENMP)
+DEFINES  = -D_GNU_SOURCE
+INCLUDES =
+LIBS     =
