711e31f73897fd535eb6f723fe887e80ba695fdd
Sriram Katta <sriram.katta.939@gmail.com>
2025-12-06 20:07:41 +0100

    Refactor config.mk to ensure MPI options are conditionally set

--- A06/skeleton/config.mk
+++ A06/skeleton/config.mk
@@ -3,6 +3,10 @@ TAG ?= ICX
 ENABLE_MPI ?= true
 ENABLE_OPENMP ?= false
 
+ifeq ($(ENABLE_MPI),true)
+OPTIONS = -D_MPI
+endif
+
 #Feature options
 OPTIONS +=  -DARRAY_ALIGNMENT=64
 OPTIONS +=  -DVERBOSE
