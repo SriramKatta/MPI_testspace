5bf138ed68809d1d4e92906fea3bd46dc5c09417
Sriram Katta <sriram.katta.939@gmail.com>
2025-12-08 21:16:46 +0100

    Add MPI_CALL macro for improved error handling in MPI calls

--- A06/skeleton/src/util.h
+++ A06/skeleton/src/util.h
@@ -19,4 +19,28 @@
 #define ABS(a) ((a) >= 0 ? (a) : -(a))
 #endif
 
+
+#define MPI_CALL(call)                                                                \
+    {                                                                                 \
+        int mpi_status = call;                                                        \
+        if (MPI_SUCCESS != mpi_status) {                                              \
+            char mpi_error_string[MPI_MAX_ERROR_STRING];                              \
+            int mpi_error_string_length = 0;                                          \
+            MPI_Error_string(mpi_status, mpi_error_string, &mpi_error_string_length); \
+            if (NULL != mpi_error_string)                                             \
+                fprintf(stderr,                                                       \
+                        "ERROR: MPI call \"%s\" in line %d of file %s failed "        \
+                        "with %s "                                                    \
+                        "(%d).\n",                                                    \
+                        #call, __LINE__, __FILE__, mpi_error_string, mpi_status);     \
+            else                                                                      \
+                fprintf(stderr,                                                       \
+                        "ERROR: MPI call \"%s\" in line %d of file %s failed "        \
+                        "with %d.\n",                                                 \
+                        #call, __LINE__, __FILE__, mpi_status);                       \
+            exit( mpi_status );                                                       \
+        }                                                                             \
+    }
+
+
 #endif // __UTIL_H_
