4837875a71716d8af41337eb8515cc95e53fd7f0
Sriram Katta <sriram.katta.939@gmail.com>
2025-12-09 22:16:43 +0100

    Add benchmark configuration files and SLURM scripts for canal flow simulations

---
+++ A07/solver_benchmark/node_single.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -l
+#
+#SBATCH --partition=singlenode
+#SBATCH --nodes=1
+#SBATCH -J single_node_scale
+#SBATCH --output=SLURM_OUT_FILES/%x_%j.out
+#SBATCH --error=SLURM_ERR_FILES/%x_%j.err
+#SBATCH --time=01:00:00
+#SBATCH --export=NONE
+
+unset SLURM_EXPORT_ENV
+
+
+module load intel
+module load intelmpi
+module load likwid
+
+DIR_NAME=benchmark_${SLURM_JOB_NAME}_${SLURM_JOB_ID}
+EXE_NAME=EXE_${SLURM_JOB_ID}
+
+mkdir -p $DIR_NAME
+cp ./exe-ICX ./${DIR_NAME}/${EXE_NAME}
+
+cd $DIR_NAME
+
+for numproc in 18 36 54 72
+do
+    mkdir -p ${numproc}_cores
+    echo "****************************************************************************************"
+    echo "running on ${numproc} core of ${SLURM_JOB_NAME}"
+    echo "****************************************************************************************"
+    cd ${numproc}_cores
+    likwid-mpirun -mpi slurm \
+                    -omp intel \
+                    -n ${numproc} \
+                    ../${EXE_NAME} ../../canal_node.par | tee resfile
+    cd ..
+done
+
+rm ${EXE_NAME}
+
